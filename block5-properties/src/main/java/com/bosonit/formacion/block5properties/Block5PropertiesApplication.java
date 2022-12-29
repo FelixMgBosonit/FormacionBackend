29921f1b6738c62a2b9ffde08e868f3062187488
Félix Muñoz <felix.munoz@bosonit.com>
2022-12-29 09:44:37 +0100

    Resubida block5-properties

---
+++ block5-properties/src/main/java/com/bosonit/formacion/block5properties/Block5PropertiesApplication.java
@@ -0,0 +1,18 @@
+package com.bosonit.formacion.block5properties;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block5PropertiesApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block5PropertiesApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
