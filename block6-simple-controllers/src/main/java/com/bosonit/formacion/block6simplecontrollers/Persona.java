1e8305f6de4ce9f65e161ba5e1a8cf5944932818
Félix Muñoz <felix.munoz@bosonit.com>
2023-01-09 09:28:06 +0100

    block6-simple-controllers

---
+++ block6-simple-controllers/src/main/java/com/bosonit/formacion/block6simplecontrollers/Persona.java
@@ -0,0 +1,10 @@
+package com.bosonit.formacion.block6simplecontrollers;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+    private String name;
+    private String ciudad;
+    private int edad;
+}
