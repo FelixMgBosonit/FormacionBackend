f4aa28f709aae318c95622b6a2ed73e1cd593936
Félix Muñoz <felix.munoz@bosonit.com>
2023-01-18 17:31:41 +0100

    block7-crud-validation 2.1

--- block7-crud-validation/src/main/java/com/bosonit/formacion/block7crudvalidation/controller/Controller.java
+++ block7-crud-validation/src/main/java/com/bosonit/formacion/block7crudvalidation/controller/Controller.java
@@ -3,6 +3,8 @@ package com.bosonit.formacion.block7crudvalidation.controller;
 import com.bosonit.formacion.block7crudvalidation.application.PersonaServiceImpl;
 import com.bosonit.formacion.block7crudvalidation.controller.dto.PersonaInputDto;
 import com.bosonit.formacion.block7crudvalidation.controller.dto.PersonaOutputDto;
+import com.bosonit.formacion.block7crudvalidation.exception.EntityNotFoundException;
+import com.bosonit.formacion.block7crudvalidation.exception.UnprocessableEntityException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,17 +22,23 @@ public class Controller {
 
 
     @PostMapping("/")
-    public ResponseEntity<PersonaOutputDto> addPersona(@RequestBody PersonaInputDto persona) throws Exception {
-
+    public ResponseEntity<PersonaOutputDto> addPersona(@RequestBody PersonaInputDto persona) {//throws Exception eliminado por los ifs de personaServiceImpl
+        try {
             return new ResponseEntity<>(personaService.addPersona(persona), HttpStatus.CREATED);
+        } catch (Exception e) {
+            throw new UnprocessableEntityException("Los campos no cumplen con los requisitos establecidos");
         }
+    }
 
 
     @GetMapping("/id/{id}")
     public ResponseEntity<PersonaOutputDto> getPersonaById(@PathVariable int id) {
-        return ResponseEntity.ok().body(personaService.getPersonaById(id));
+        try {
+            return ResponseEntity.ok().body(personaService.getPersonaById(id));
+        } catch (Exception e) {
+            throw new EntityNotFoundException("Usuario no encontrado");
+        }
     }
-
 
     @GetMapping("/usuario/{usuario}")
     public ResponseEntity<PersonaOutputDto> getPersonaByUserName(@PathVariable String usuario) {
